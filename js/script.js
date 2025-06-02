9ce355208e99df5e1d1d27510992028b8c314883
Md Rihanul kabir <rihankabir006@gmail.com>
2025-06-02 23:12:15 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+  //  form validation
+  (() => {
+    'use strict'
+
+    const form = document.getElementById('contactForm')
+
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+      form.classList.add('was-validated')
+    }, false)
+  })()
